ae0a947644659c4d4f4116cb072dc387e154e790
joker <547402844@qq.com>
2021-12-17 13:42:35 +0800

    Joker => swagger 3.0.0

--- joker-swagger2/src/main/java/com/joker/jokerswagger2/JokerSwagger2Application.java
+++ joker-swagger2/src/main/java/com/joker/jokerswagger2/JokerSwagger2Application.java
@@ -2,10 +2,10 @@ package com.joker.jokerswagger2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class JokerSwagger2Application {
 
     public static void main(String[] args) {
--- joker-swagger2/src/main/java/com/joker/jokerswagger2/utils/Swagger2.java
+++ joker-swagger2/src/main/java/com/joker/jokerswagger2/utils/Swagger2.java
@@ -1,21 +1,23 @@
 package com.joker.jokerswagger2.utils;
 
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /***
  *      swagger2    配置类
  */
 @Configuration
-@EnableSwagger2
+//@EnableSwagger2
+@EnableOpenApi
 public class Swagger2 {
 
     /**
@@ -24,15 +26,28 @@ public class Swagger2 {
      * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
      * @return
      */
+//    @Bean
+//    public Docket createRestApi(){
+//     return   new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.joker.jokerswagger2.controller"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+
     @Bean
-    public Docket createRestApi(){
-     return   new Docket(DocumentationType.SWAGGER_2)
+
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.joker.jokerswagger2.controller"))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build();
     }
+
+
 
     /**
      * 创建API的基本信息
