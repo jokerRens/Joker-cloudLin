d09de8cc9fd8cc5ca6fa45d1deea939eefa5767e
joker <547402844@qq.com>
2022-03-14 09:49:32 +0800

    新增: 启动类

---
+++ joker-designmode/src/test/java/com/joker/designmode/DesignmodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joker.designmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignmodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
