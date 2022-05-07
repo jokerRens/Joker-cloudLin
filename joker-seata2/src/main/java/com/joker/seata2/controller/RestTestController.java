d63c0765db8c22572017f3edd8f48ab8a27d31ca
joker <547402844@qq.com>
2022-05-07 18:03:49 +0800

    demo

--- joker-seata2/src/main/java/com/joker/seata2/controller/RestTestController.java
+++ joker-seata2/src/main/java/com/joker/seata2/controller/RestTestController.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
+import java.text.DecimalFormat;
+import java.util.*;
 
 /**
  * RestController
@@ -25,6 +26,73 @@ public class RestTestController {
 
     @Autowired
     RestTemplate restTemplate;
+
+    public static void main(String[] args) {
+//        String ss = "aacabdkacaa";
+
+        String s = longestPalindrome("ccc");
+        System.out.println(s);
+    }
+
+
+    public static String longestPalindrome(String s) {
+        String substring = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s2 = s.substring(i, s.length());
+            int indexOf = s2.indexOf(s.charAt(i), s2.indexOf(s.charAt(i)) + 1);
+            if(indexOf==-1 && i==0){
+                substring = s2.charAt(0)+"";
+            }
+            if(indexOf+i!=i){
+                String s1 = s.substring(i, indexOf + 1 + i);
+                if(s1.length()>substring.length()){
+                    substring = s1;
+                }
+            }
+        }
+        return substring;
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums = new int[nums1.length+nums2.length];
+        System.arraycopy(nums1,0,nums,0,nums1.length);
+        System.arraycopy(nums2,0,nums,nums1.length,nums2.length);
+        Arrays.sort(nums);
+        if(nums.length%2==0){
+            double a = (float)(nums[(nums.length / 2) - 1] + nums[(nums.length / 2)]) / 2;
+            return a;
+        }else {
+            return nums[nums.length/2];
+        }
+    }
+
+
+    public static String longestCommonPrefix(String[] strs) {
+        String temp = "";
+        for (int i = 0; i < strs.length; i++) {
+            String str = strs[i];
+            for (int j = 0; i < str.length(); i++) {
+                char charAt = str.charAt(j);
+
+            }
+        }
+        return temp;
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int size = 2;
+        int[] ints = new int[size];
+        for (int i = 0; i < nums.length; i++) {
+            for (int i1 = 1; i1 < nums.length; i1++) {
+                if(nums[i]+nums[i1]==target && i1>i){
+                    ints[0] = i;
+                    ints[1] = i1;
+                    return ints;
+                }
+            }
+        }
+        return null;
+    }
 
 
     @GetMapping("/test1")
